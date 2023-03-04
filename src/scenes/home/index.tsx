ea0583679323d0a2ccd2383d6b2d089d73806943
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-04 11:07:48 -0800

    inital layout of home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,19 +1,50 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
-import HomePageText from "@/assets/HomePageText.png"
-import HomePageGraphic from "@/assets/HomePageGraphic.png"
-import SponsorRedBull from "@/assets/SponsorRedBull.png"
-import SponsorForbes from "@/assets/SponsorForbes.png"
-import SponsorFortune from "@/assets/SponsorFortune.png"
-
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage }: Props) => {
-  return <div>Home</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE & MAIN HEADER */}
+      <div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+            <p>
+              Unrivaled gym. Unparalleled training fitness classes. World class
+              studios to ACHIEVE the fitness level that you DREAM of!
+            </p>
+          </div>
+          {/* ACTIONS */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"></AnchorLink>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;

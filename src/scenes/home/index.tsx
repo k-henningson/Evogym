69b91e6c0daf50369125111b7a96b3b2c2a7d0bf
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-04 17:56:50 -0800

    stagger animation of buttons to delay after logo

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -44,7 +44,17 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
           {/* ACTIONS */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -55,7 +65,7 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         {/* IMAGE */}
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">

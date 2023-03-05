546010e5b5a1a8ed2d7e5c93a76abbf704121ba4
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-05 10:09:06 -0800

    set benefits page upon scroll, import framer motion

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -4,13 +4,24 @@ import {
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Benefits = ({ setSelectedPage }: Props) => {
-  return <div>Benefits</div>;
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <div>
+         <h1 className="basis-3/5 font-montserrat text-3xl font-bold"></h1>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Benefits;

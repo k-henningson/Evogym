ffd7b557e2173e84fc62ac99a9762ceb024eedaa
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-05 13:49:11 -0800

    make benefits header slide in from left of screen

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -44,14 +44,24 @@ const Benefits = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* HEADER */}
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST A GYM.</HText>
           <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers, and classes to
             get you to your ultimate fitness goals with ease. Individual
             tailored care for each and every member.
           </p>
-        </div>
+        </motion.div>
         {/* BENEFITS */}
         <motion.div
           className="mt-5 items-center justify-between gap-8 md:flex"

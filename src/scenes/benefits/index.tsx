2d40e8a4c45aefa209d930e2fc2aa86ed7283681
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-05 10:24:02 -0800

    build out benefits section of page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,6 +7,24 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description: "Change this description later",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Change this description later",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description: "Change this description later",
+  },
+]
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -17,8 +35,20 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
           <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers, and classes to
+            get you to your ultimate fitness goals with ease. Individual
+            tailored care for each and every member.
+          </p>
+        </div>
+        {/* BENEFITS */}
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+          {benefits.map((benefit) => (
+          <Benefit />
+          ))}
         </div>
       </motion.div>
     </section>

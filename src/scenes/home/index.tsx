5f5ff7239f30bd1260dfbb406b061a70f2140797
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-04 17:22:12 -0800

    make sponsor logos and main image responsive

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -27,13 +27,13 @@ const Home = ({ setSelectedPage }: Props) => {
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm">
               Unrivaled gym. Unparalleled training fitness classes. World class
               studios to ACHIEVE the fitness level that you DREAM of!
             </p>
           </div>
           {/* ACTIONS */}
-          <div>
+          <div className="mt-8 flex items-center gap-8">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -47,15 +47,15 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
         </div>
         {/* IMAGE */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </div>
       {/* SPONSORS */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h=[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img alt="redbull-sponsor" src={SponsorRedBull} />
               <img alt="forbes-sponsor" src={SponsorForbes} />
               <img alt="fortune-sponsor" src={SponsorFortune} />

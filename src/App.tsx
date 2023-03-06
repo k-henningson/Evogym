ce96e90b7db34a542683b9d4dea24692e17b2746
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-05 16:15:28 -0800

    create footer and add lorem ipsum text to fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
-import OurClasses from "@/scenes/ourClasses"
+import ContactUs from "@/scenes/contactUs";
+import OurClasses from "@/scenes/ourClasses";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -35,6 +37,8 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
